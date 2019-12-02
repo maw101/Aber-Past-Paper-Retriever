21f7eb99db850a56df39b3016ff9d5f15793f330
maw101 <maw101@aber.ac.uk>
2019-12-02 21:49:59 +0000

    Corrected format_pdf_url such that it now takes a generic department

--- aber_past_paper_retriever.py
+++ aber_past_paper_retriever.py
@@ -1,7 +1,9 @@
 import os, requests, getpass
 
-def format_pdf_url(year, semester, module_code):
-    return f'https://www.aber.ac.uk/en/media/departmental/examinations/pastpapers/pdf/compsci/sem{semester}-{year % 100}/{module_code}-{year % 100}.pdf'
+DEPARTMENT = 'compsci'
+
+def format_pdf_url(year, semester, module_code, department):
+    return f'https://www.aber.ac.uk/en/media/departmental/examinations/pastpapers/pdf/{department}/sem{semester}-{year % 100}/{module_code}-{year % 100}.pdf'
 
 
 def format_local_pdf_path(year, semester, module_code):
