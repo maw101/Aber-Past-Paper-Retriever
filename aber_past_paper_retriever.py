e90966086be3b04e11cd4fd6f2bc7d4f54d54d7e
maw101 <maw101@aber.ac.uk>
2019-12-02 21:44:30 +0000

    Added method to print the result of a paper retrieval operation

--- aber_past_paper_retriever.py
+++ aber_past_paper_retriever.py
@@ -9,6 +9,8 @@ def format_local_pdf_path(year, semester, module_code):
     output_file_path = os.path.join(module_code, output_file_name)
     return output_file_path;
 
+def print_formatted_retrieval_result(year, semester, module_code, result):
+    print(module_code, year, "Semester", semester, result)
 
 def get_auth_header():
     # get username and password for authentication
