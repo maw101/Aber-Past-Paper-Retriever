9ca2e071cab4d06b1a175606cb4f436d9beb7e35
maw101 <maw101@aber.ac.uk>
2019-12-02 21:43:56 +0000

    Added method to get Aber credentials as user input

--- aber_past_paper_retriever.py
+++ aber_past_paper_retriever.py
@@ -1,4 +1,4 @@
-import os
+import os, requests, getpass
 
 def format_pdf_url(year, semester, module_code):
     return f'https://www.aber.ac.uk/en/media/departmental/examinations/pastpapers/pdf/compsci/sem{semester}-{year % 100}/{module_code}-{year % 100}.pdf'
@@ -8,3 +8,15 @@ def format_local_pdf_path(year, semester, module_code):
     output_file_name = f'{module_code}-{year}-{semester}.pdf'
     output_file_path = os.path.join(module_code, output_file_name)
     return output_file_path;
+
+
+def get_auth_header():
+    # get username and password for authentication
+    USERNAME = getpass.getpass("Enter Aberystwyth Username: ")
+    
+    try: 
+        PASSWORD = getpass.getpass("Enter Aberystwyth Password: ")
+    except Exception as error: 
+        print('ERROR', error)
+        
+    return requests.auth.HTTPBasicAuth(USERNAME, PASSWORD)
