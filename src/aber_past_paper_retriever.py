de18aafed2e77f4be75f9c1e22b68c1a47b10f58
maw101 <maw101@aber.ac.uk>
2020-06-08 15:04:02 +0100

    Added setter for department url model attribute

--- src/aber_past_paper_retriever.py
+++ src/aber_past_paper_retriever.py
@@ -28,3 +28,20 @@ class PaperRetriever:
         # encode HTTPBasicAuth header
         self.auth_header = requests.auth.HTTPBasicAuth(username, password)
 
+    def set_department_url(self, department_url):
+        """Sets the department URL attribute.
+
+        Attributes:
+            department_url (str): the new value for the department URL attribute
+
+        """
+        self.department_url = department_url
+        
+        # check to see if we are on the Business School intermediary graduate level page
+        if self.department_url[-6:] == '/smba/':
+            if self.graduate_level == 'Undergraduate':
+                self.department_url = self.department_url + 'ugrad/'
+            else:
+                self.department_url = self.department_url + 'postg/'
+
+    
