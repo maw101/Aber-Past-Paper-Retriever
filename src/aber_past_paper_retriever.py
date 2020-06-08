3d5c85363f0c04cff3ea26bd3ddd332d09804e58
maw101 <maw101@aber.ac.uk>
2020-06-08 15:03:03 +0100

    Defined base retriever class in model

--- src/aber_past_paper_retriever.py
+++ src/aber_past_paper_retriever.py
@@ -4,3 +4,17 @@ import lxml.html
 
 WEBSITE_BASE_URL = 'https://www.aber.ac.uk'
 DEPARTMENT_LISTING_URL = WEBSITE_BASE_URL + '/en/past-papers/'
+
+class PaperRetriever:
+    """Represents a Past Paper Retriever from the Aber Uni Website.
+
+    Attributes:
+        auth_header (HTTPBasicAuth Header): stores user credential header
+        department_url (str): URL for the department
+        graduate_level (str): the graduate level of the user
+        destination_folder (str): a path for the output folder for PDF's found
+        module_code (str): the 7 character module code
+
+    """
+    
+    
